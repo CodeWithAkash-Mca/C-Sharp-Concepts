cd020699cb87abb3e5b15bf75349a7b859652de8
Akash Yadav <aakasyadav358@gmail.com>
2026-09-04 19:52:44 +0530

    frontend file added

---
+++ Default.aspx
@@ -0,0 +1,270 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+
+<form id="form1" runat="server">
+
+    <h1>Library Management System</h1>
+
+    <hr />
+
+    <h2>Add / Update Book</h2>
+
+    <p>
+        Book ID:
+        <asp:TextBox ID="txtBookId" runat="server"></asp:TextBox>
+    </p>
+
+    <p>
+        Title:
+        <asp:TextBox ID="txtTitle" runat="server"></asp:TextBox>
+    </p>
+
+    <p>
+        Author:
+        <asp:TextBox ID="txtAuthor" runat="server"></asp:TextBox>
+    </p>
+
+    <p>
+        Price:
+        <asp:TextBox ID="txtPrice" runat="server"></asp:TextBox>
+    </p>
+
+    <p>
+        Category:
+
+        <asp:DropDownList ID="ddlCategory" runat="server">
+
+            <asp:ListItem Text="Select Category"
+                Value=""></asp:ListItem>
+
+            <asp:ListItem Text="Hindi Literature"
+                Value="Hindi Literature"></asp:ListItem>
+
+            <asp:ListItem Text="Poetry"
+                Value="Poetry"></asp:ListItem>
+
+            <asp:ListItem Text="Satire"
+                Value="Satire"></asp:ListItem>
+
+            <asp:ListItem Text="Novel"
+                Value="Novel"></asp:ListItem>
+
+            <asp:ListItem Text="Drama"
+                Value="Drama"></asp:ListItem>
+
+            <asp:ListItem Text="Historical Fiction"
+                Value="Historical Fiction"></asp:ListItem>
+
+        </asp:DropDownList>
+    </p>
+
+    <p>
+        <asp:Button ID="btnAdd"
+            runat="server"
+            Text="Add Book"
+            OnClick="btnAdd_Click" />
+
+        <asp:Button ID="btnUpdate"
+            runat="server"
+            Text="Update Book"
+            OnClick="btnUpdate_Click" />
+    </p>
+
+
+    <hr />
+
+    <h2>Search Book</h2>
+
+    <p>
+        Enter Book ID:
+
+        <asp:TextBox ID="txtSearch"
+            runat="server"></asp:TextBox>
+
+        <asp:Button ID="btnSearch"
+            runat="server"
+            Text="Search"
+            OnClick="btnSearch_Click" />
+    </p>
+
+
+    <hr />
+
+    <h2>Remove Book</h2>
+
+    <p>
+        Book ID:
+
+        <asp:TextBox ID="txtDeleteId"
+            runat="server"></asp:TextBox>
+
+        <asp:Button ID="btnDelete"
+            runat="server"
+            Text="Delete"
+            OnClick="btnDelete_Click" />
+    </p>
+
+
+    <hr />
+
+    <h2>Find Books</h2>
+
+    <p>
+
+        Select Criteria:
+
+        <asp:DropDownList ID="ddlCriteria"
+            runat="server"
+            AutoPostBack="true"
+            OnSelectedIndexChanged="ddlCriteria_SelectedIndexChanged">
+
+            <asp:ListItem Text="Select Criteria"
+                Value=""></asp:ListItem>
+
+            <asp:ListItem Text="By Author"
+                Value="Author"></asp:ListItem>
+
+            <asp:ListItem Text="By Category"
+                Value="Category"></asp:ListItem>
+
+            <asp:ListItem Text="Price Above"
+                Value="Price"></asp:ListItem>
+
+        </asp:DropDownList>
+
+    </p>
+
+    <p>
+        <asp:DropDownList ID="ddlAuthor"
+            runat="server"
+            Visible="false">
+
+            <asp:ListItem Text="Select Author"
+                Value=""></asp:ListItem>
+
+            <asp:ListItem Text="Munshi Premchand"
+                Value="Munshi Premchand"></asp:ListItem>
+
+            <asp:ListItem Text="Harivansh Rai Bachchan"
+                Value="Harivansh Rai Bachchan"></asp:ListItem>
+
+            <asp:ListItem Text="Shrilal Shukla"
+                Value="Shrilal Shukla"></asp:ListItem>
+
+            <asp:ListItem Text="Phanishwar Nath Renu"
+                Value="Phanishwar Nath Renu"></asp:ListItem>
+
+            <asp:ListItem Text="Mahadevi Varma"
+                Value="Mahadevi Varma"></asp:ListItem>
+
+        </asp:DropDownList>
+    </p>
+
+    <p>
+        <asp:DropDownList ID="ddlSearchCategory"
+            runat="server"
+            Visible="false">
+
+            <asp:ListItem Text="Select Category"
+                Value=""></asp:ListItem>
+
+            <asp:ListItem Text="Hindi Literature"
+                Value="Hindi Literature"></asp:ListItem>
+
+            <asp:ListItem Text="Poetry"
+                Value="Poetry"></asp:ListItem>
+
+            <asp:ListItem Text="Satire"
+                Value="Satire"></asp:ListItem>
+
+            <asp:ListItem Text="Novel"
+                Value="Novel"></asp:ListItem>
+
+            <asp:ListItem Text="Drama"
+                Value="Drama"></asp:ListItem>
+
+            <asp:ListItem Text="Historical Fiction"
+                Value="Historical Fiction"></asp:ListItem>
+
+        </asp:DropDownList>
+    </p>
+
+
+    <!-- PRICE -->
+
+    <p>
+
+        <asp:TextBox ID="txtMinimumPrice"
+            runat="server"
+            Visible="false"></asp:TextBox>
+
+    </p>
+
+
+    <p>
+        <asp:Button ID="btnFind"
+            runat="server"
+            Text="Find Books"
+            OnClick="btnFind_Click" />
+    </p>
+
+
+    <hr />
+
+    <h2>Sort Books</h2>
+
+    <p>
+
+        Sort By:
+
+        <asp:DropDownList ID="ddlSort"
+            runat="server">
+
+            <asp:ListItem Text="Select Sorting"
+                Value=""></asp:ListItem>
+
+            <asp:ListItem Text="Title - Ascending"
+                Value="TitleAsc"></asp:ListItem>
+
+            <asp:ListItem Text="Title - Descending"
+                Value="TitleDesc"></asp:ListItem>
+
+        </asp:DropDownList>
+
+        <asp:Button ID="btnSort"
+            runat="server"
+            Text="Sort"
+            OnClick="btnSort_Click" />
+
+    </p>
+
+
+    <hr />
+
+    <h2>All Books</h2>
+
+    <asp:Button ID="btnDisplay"
+        runat="server"
+        Text="Display All Books"
+        OnClick="btnDisplay_Click" />
+
+    <p>
+        <asp:Label ID="lblMessage"
+            runat="server"></asp:Label>
+    </p>
+
+    <asp:Literal ID="litBooks"
+        runat="server"></asp:Literal>
+
+
+</form>
+
+</body>
+</html>
---
+++ Default.aspx.cs
@@ -0,0 +1,270 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using LibraryManagementSystemLib;
+
+public partial class _Default : System.Web.UI.Page
+{
+    // getting list from class lib
+    List<Book> books = Library.GetBooks();
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void btnAdd_Click(object sender, EventArgs e)
+    {
+        try
+        {
+            int id = Convert.ToInt32(txtBookId.Text);
+            decimal price = Convert.ToDecimal(txtPrice.Text);
+    // Check whether Book ID already exists
+
+            bool exists = books.Exists(b => b.BookId == id);
+
+            if (exists)
+            {
+                lblMessage.Text = "Book ID already exists.";
+                return;
+            }
+
+            books.Add(new Book { BookId = id, Author = txtAuthor.Text, Category = ddlCategory.SelectedValue, Price = price, Title = txtTitle.Text });
+           lblMessage.Text = "Book added successfully."; 
+        }
+        catch
+        {
+            lblMessage.Text = "Please enter valid details.";
+        }
+    }
+
+    protected void btnUpdate_Click(object sender, EventArgs e)
+    {
+        try
+        {
+            int id = Convert.ToInt32(txtBookId.Text);
+            decimal price = Convert.ToDecimal(txtPrice.Text);
+          Book book = books.Find(b => b.BookId == id);
+
+            if (book != null)
+            {
+                book.Title = txtTitle.Text;
+                book.Author = txtAuthor.Text;
+                book.Price = price;
+                book.Category = ddlCategory.SelectedValue;
+
+                lblMessage.Text = "Book updated successfully.";
+
+                DisplayBooks(books);
+            }
+            else
+            {
+                lblMessage.Text = "Book not found.";
+            }
+        }
+        catch
+        {
+            lblMessage.Text = "Please enter valid details.";
+        }
+    }
+
+    protected void btnSearch_Click(object sender, EventArgs e)
+    {
+        int id = Convert.ToInt32(txtSearch.Text);
+        
+       Book book = books.Find(b => b.BookId == id);
+
+        if (book != null)
+        {
+            List<Book> result = new List<Book>();
+
+            result.Add(book);
+
+            lblMessage.Text = "Book found.";
+
+            DisplayBooks(result);
+        }
+        else
+        {
+            lblMessage.Text = "Book not found.";
+        }
+    }
+  protected void btnDelete_Click(object sender, EventArgs e)
+    {
+        try
+        {
+            int id = Convert.ToInt32(txtDeleteId.Text);
+            Book book = books.Find(b => b.BookId == id);
+
+            if (book != null)
+            {
+                books.Remove(book);
+
+                lblMessage.Text = "Book deleted successfully.";
+
+                DisplayBooks(books);
+            }
+            else
+            {
+                lblMessage.Text = "Book not found.";
+            }
+        }
+        catch
+        {
+            lblMessage.Text = "Please enter valid Book ID.";
+        }
+    }
+
+    protected void ddlCriteria_SelectedIndexChanged(
+        object sender,
+        EventArgs e)
+    {// according to the selection ddl appear
+        ddlAuthor.Visible = false;
+        ddlSearchCategory.Visible = false;
+        txtMinimumPrice.Visible = false;
+
+        if (ddlCriteria.SelectedValue == "Author")
+        {
+            ddlAuthor.Visible = true;
+        }
+        else if (ddlCriteria.SelectedValue == "Category")
+        {
+            ddlSearchCategory.Visible = true;
+        }
+        else if (ddlCriteria.SelectedValue == "Price")
+        {
+            txtMinimumPrice.Visible = true;
+        }
+    }
+    protected void btnFind_Click(object sender, EventArgs e)
+    {
+
+        List<Book> result = new List<Book>();
+
+
+        // FIND BY AUTHOR
+
+        if (ddlCriteria.SelectedValue == "Author")
+        {
+            string author = ddlAuthor.SelectedValue;
+
+            result = books.FindAll(
+                b => b.Author == author
+            );
+        }
+
+
+        // FIND BY CATEGORY
+
+        else if (ddlCriteria.SelectedValue == "Category")
+        {
+            string category = ddlSearchCategory.SelectedValue;
+
+            result = books.FindAll(
+                b => b.Category == category
+            );
+        }
+
+
+        // FIND BY PRICE
+
+        else if (ddlCriteria.SelectedValue == "Price")
+        {
+            try
+            {
+                decimal price =
+                    Convert.ToDecimal(txtMinimumPrice.Text);
+
+                result = books.FindAll(
+                    b => b.Price > price
+                );
+            }
+            catch
+            {
+                lblMessage.Text = "Please enter valid price.";
+                return;
+            }
+        }
+        else
+        {
+            lblMessage.Text = "Please select criteria.";
+            return;
+        }
+
+
+        if (result.Count > 0)
+        {
+            lblMessage.Text =
+                "Books found: " + result.Count;
+
+            DisplayBooks(result);
+        }
+        else
+        {
+            lblMessage.Text = "No books found.";
+        }
+    }
+
+
+    protected void btnSort_Click(object sender, EventArgs e)
+    {
+      
+        if (ddlSort.SelectedValue == "TitleAsc")
+        {
+            books.Sort(
+                (a, b) => a.Title.CompareTo(b.Title)
+            );
+
+            lblMessage.Text =
+                "Books sorted by title ascending.";
+        }
+        else if (ddlSort.SelectedValue == "TitleDesc")
+        {
+            books.Sort(
+                (a, b) => b.Title.CompareTo(a.Title)
+            );
+
+            lblMessage.Text =
+                "Books sorted by title descending.";
+        }
+        else
+        {
+            lblMessage.Text =
+                "Please select sorting.";
+
+            return;
+        }
+
+        DisplayBooks(books);
+    }
+    
+    protected void btnDisplay_Click(object sender, EventArgs e)
+    {
+      
+
+        lblMessage.Text =
+            "Total Books: " + books.Count;
+
+        DisplayBooks(books);
+    }
+
+    private void DisplayBooks(List<Book> books)
+    {
+        foreach (Book book in books)
+        {
+            Response.Write("Book ID: " + book.BookId + "<br />");
+
+            Response.Write("Title: " + book.Title + "<br />");
+
+            Response.Write("Author: " + book.Author + "<br />");
+
+            Response.Write("Price: " + book.Price + "<br />");
+
+            Response.Write("Category: " + book.Category + "<br />");
+
+            Response.Write("<hr />");
+        }
+    }
+}
