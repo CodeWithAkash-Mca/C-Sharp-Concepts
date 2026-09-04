8e058a62da1b57fdbb6829b35a272fc6e71ffeae
Akash Yadav <aakasyadav358@gmail.com>
2026-09-04 19:52:09 +0530

    class files of Library management

---
+++ Book.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagementSystemLib
+{
+    /// <summary>
+    /// this class represents the real world book entity 
+    /// </summary>
+    public class Book
+    {
+        public int BookId { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public decimal Price { get; set; }
+        public string Category { get; set; }
+    }
+}
